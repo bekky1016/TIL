c0a8bf0609cf64641ef376310747886d2aa55f6d
bekky1016 <bekky1016@gmail.com>
2025-08-21 16:52:07 +0900

    til test 20250821

---
+++ python/coding_test_2.py
@@ -0,0 +1,14 @@
+
+# before Comprehension
+old_list = [1,2,3,4,5]
+result = []
+
+for i in old_list:
+    if i % 2 == 0:
+	result.append(i)
+
+print(result)
+
+# with Comprehension
+result = [i for i in old_list if i % 2 == 0]
+print(result)
